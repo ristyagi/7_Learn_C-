36f8659286031cdc9e9b6dc090d13d4611228d55
Rishabh Tyagi <30928799+ristyagi@users.noreply.github.com>
2024-08-20 17:52:18 +0530

    Create hello_C++.cpp

---
+++ hello_C++.cpp
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=ZzaPdXTrSb8
+// main function is the entry point of a program in C++
+// c++ is case sensatie language
+// before function name we should define the datatype of value it is going to return
+// # include helps us include one or more of the files from standard library that can be used in our program
+// to run a C++ program it has to be first complied into machine code that can be run by computer's operating system.
+// if code is complied on win os then it will be converted in an excutable and that will run on windows only and not on mac or linux. Same happens with other OS
+
+
+#include <iostream>
+
+int main() {
+    // std::cout << "\n";
+    std::cout << "Hello World";
+    // std::cout << "\n\n";
+    return 0;
+}
+
+// cout - character out not console out, using this object we can ouput one or more character onf screen
